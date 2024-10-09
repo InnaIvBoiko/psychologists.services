58ca7ddf30a1a973e8804cfb7bd260f06ecbce15
InnaIvBoiko <inna_boiko@libero.it>
2024-10-09 18:44:07 +0200

    add Load more button, spinners

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,15 @@
 import { Suspense } from 'react';
+import { RotateSpinner } from 'react-spinners-kit';
 import Navigation from '../Navigation/Navigation';
 import css from './Layout.module.css';
 
-export default function Layout({children}) {
+export default function Layout({children = null}) {
     return (
         <div className={css.container}>
             <Navigation />
-            <Suspense fallback={<h3>Please wait loading page...</h3>} >
+            <Suspense fallback={<div className={css.spinner}>
+                <RotateSpinner size={120} color='#54be96' />
+            </div>} >
                 {children}
             </Suspense>
         </div>
--- src/pages/PsychologistsPage/PsychologistsPage.jsx
+++ src/pages/PsychologistsPage/PsychologistsPage.jsx
@@ -1,29 +1,74 @@
 import { useEffect, useState } from 'react';
+import { PulseSpinner } from 'react-spinners-kit';
 import Filters from '../../components/Filters/Filters.jsx';
 import PsychologistsList from '../../components/PsychologistsList/PsychologistsList.jsx';
 import { db } from "../../utils/firebase";
-import { off, onValue, ref } from "firebase/database";
+import { ref, onValue, query, limitToFirst, orderByKey, startAfter } from "firebase/database";
 import css from './PsychologistsPage.module.css';
 
 export default function PsychologistsPage() {
-    const [psychologists, setPsychologists] = useState(null); 
-    const [loading, setLoading] = useState(true);
+    const [psychologists, setPsychologists] = useState([]); 
+    const [lastKey, setLastKey] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [isEnd, setIsEnd] = useState(false);
+
+    const itemsPerPage = 3; 
+
+    const loadInitialData = () => {
+        setLoading(true);
+        const psychologistsQuery = query(ref(db, 'psychologists'), orderByKey(), limitToFirst(itemsPerPage));
+        
+        onValue(psychologistsQuery, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                const fetchedData = Object.entries(data).map(([key, value]) => ({ key, ...value }));
+                setPsychologists(fetchedData); 
+                setLastKey(fetchedData[fetchedData.length - 1].key);
+            } else {
+                setIsEnd(true);
+            }
+            setLoading(false);
+        });
+    };
+
+    const loadMoreData = () => {
+        if (lastKey) {
+            setLoading(true);
+            const psychologistsQuery = query(
+                ref(db, 'psychologists'),
+                orderByKey(),
+                startAfter(lastKey), 
+                limitToFirst(itemsPerPage) 
+            );
+
+            onValue(psychologistsQuery, (snapshot) => {
+                const data = snapshot.val();
+                if (data) {
+                    const fetchedData = Object.entries(data).map(([key, value]) => ({ key, ...value }));
+                    setPsychologists(prev => [...prev, ...fetchedData]); 
+                    setLastKey(fetchedData[fetchedData.length - 1].key); 
+                } else {
+                    setIsEnd(true);
+                }
+                setLoading(false);
+            });
+        }
+    };
 
     useEffect(() => {
-        const query = ref(db, 'psychologists');
-        onValue(query, (snapshot) => {
-            const fetchedData = snapshot.val();
-            setPsychologists(fetchedData);
-            setLoading(false); 
-        });
-        return () => off(query); 
+        loadInitialData();
     }, []);
 
     return (
         <section className={css.container}>
-            {loading && <div>Loading...</div>}
             <Filters />
+            {loading && (<div className={css.spinner}>
+                <PulseSpinner size={60} color='#54be96'/>
+            </div>)}
             {psychologists && <PsychologistsList psychologists={psychologists} />}
+            {!isEnd && (
+                <button type='button' className={css.loadMoreBtn} onClick={loadMoreData}>Load more</button>
+            )}
         </section>
     );
 }
